f05ed7acf32182cddbd59f3fa68ae9574ede88f4
chethan-marideva <chethan.marideva@gmail.com>
2023-09-22 23:30:49 +0530

    Create SampleWithAsync.cs

---
+++ SampleWithAsync.cs
@@ -0,0 +1,18 @@
+public async Task<User> GetUserAsync(int userId)
+{
+    // Code omitted:
+    //
+    // Given a user Id {userId}, retrieves a User object corresponding
+    // to the entry in the database with {userId} as its Id.
+}
+
+public static async Task<IEnumerable<User>> GetUsersAsync(IEnumerable<int> userIds)
+{
+    var getUserTasks = new List<Task<User>>();
+    foreach (int userId in userIds)
+    {
+        getUserTasks.Add(GetUserAsync(userId));
+    }
+
+    return await Task.WhenAll(getUserTasks);
+}
